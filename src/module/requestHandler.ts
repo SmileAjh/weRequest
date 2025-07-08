ae0ead6e36cb51bec700d1e1663f2860e23ee8fd
allenan <allenan@tencent.com>
2025-07-09 07:13:13 +0800

    feat: 已经在使用备用域名，再切换其他备份域名时也回调backupDomainEnableCallback

--- src/module/requestHandler.ts
+++ src/module/requestHandler.ts
@@ -388,6 +388,9 @@ function enableBackupDomain(targetUrl: string = "") {
         // 域名备份关系：a:b b:c 如已更换一次域名b，b域名也不可用，去尝试c，则urlPerfix更新为b，保证后续不请求a域名
         if (typeof config.urlPerfix === 'string' && targetUrl.indexOf(config.urlPerfix) === -1) {
             config.urlPerfix = url.replaceDomain(config.urlPerfix);
+            if (typeof config.backupDomainEnableCallback === 'function') {
+                config.backupDomainEnableCallback(targetUrl);
+            }
         }
     }
 }
